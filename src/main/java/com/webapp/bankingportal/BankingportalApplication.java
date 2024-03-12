b0a0d8b9d8398cc4841550c923330d5f3d2180c1
Mr Singh <santoshsingh1988@gmail.com>
2024-03-12 04:10:55 +0000

    Reseting project.

--- src/main/java/com/webapp/bankingportal/BankingportalApplication.java
+++ src/main/java/com/webapp/bankingportal/BankingportalApplication.java
@@ -1,29 +1,11 @@
 package com.webapp.bankingportal;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@SpringBootApplication
-@EnableCaching // Add this annotation to enable caching support
-@EnableAsync
-@EnableSwagger2
 public class BankingportalApplication {
 
 	public static void main(String[] args) {
+		System.out.print("Test App Started.");
 		SpringApplication.run(BankingportalApplication.class, args);
-	}
-
-	@Bean
-	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.webapp.bankingportal.controller")).build();
 	}
 }
--- src/main/java/com/webapp/bankingportal/service/DashboardService.java
+++
@@ -1,9 +0,0 @@
-package com.webapp.bankingportal.service;
-
-import com.webapp.bankingportal.dto.AccountResponse;
-import com.webapp.bankingportal.dto.UserResponse;
-
-public interface DashboardService {
-    UserResponse getUserDetails(String accountNumber);
-    AccountResponse getAccountDetails(String accountNumber);
-}
